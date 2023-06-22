894388caaa4e82d1d8ccd0bf6588718068540af1
Bhargavi Mopuru <106804698+MBhargavi235@users.noreply.github.com>
2023-06-22 20:14:23 +0530

    Create EscalationsScreen.js

---
+++ 2. Navigation & Tab bar/components/EscalationsScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+const EscalationsScreen=()=>{
+  return(
+  <View>
+  <Text>Escalations Screen</Text>
+  </View>
+  )
+}
+
+
+export default EscalationsScreen
